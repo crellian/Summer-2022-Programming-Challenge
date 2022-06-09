512286a262c4d8c194a96f60614b4bb914fcface
crellian <31911863+crellian@users.noreply.github.com>
2022-06-08 23:53:08 -0700

    bugs fixed

--- scripts/sentiment_google.py
+++ scripts/sentiment_google.py
@@ -1,7 +1,5 @@
 import os
-#from google.cloud import language_v1
-from flair.models import TextClassifier
-from flair.data import Sentence
+from google.cloud import language_v1
 from tqdm import tqdm
 import plotly.express as px
 import chardet
@@ -9,8 +7,7 @@ import json
 import re
 import time
 
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../credential/japanese-ocr-337002-9b078adb6791.json'
-sia = TextClassifier.load('en-sentiment')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../credential/japanese-ocr-337002-9b078adb6791.json'
 
 def isEnglish(l):
     for d in l:
@@ -25,7 +22,7 @@ def countSentence(l):
     for d in l:
         count += len(d['article'])
     return count
-'''
+
 def analyze_sentiment(text_content):
     client = language_v1.LanguageServiceClient()
     type_ = language_v1.Document.Type.PLAIN_TEXT
@@ -34,17 +31,6 @@ def analyze_sentiment(text_content):
     response = client.analyze_sentiment(request = {'document': document})
     
     return response.document_sentiment.score
-'''
-def analyze_sentiment_(text_content):
-    sentence = Sentence(text_content)
-    sia.predict(sentence)
-    label = sentence.labels[0]
-    if "POSITIVE" in label.value:
-        return label.score
-    elif "NEGATIVE" in label.value:
-        return -label.score
-    else:
-        return 0
 
 #Read json file
 f = open('../news.json')
@@ -58,11 +44,11 @@ scores = []
 start  = time.time()
 
 for text in news:
-    title_score  = analyze_sentiment_(text["title"])
-    subhead_score = analyze_sentiment_(text["subhead"])
+    title_score  = analyze_sentiment(text["title"])
+    subhead_score = analyze_sentiment(text["subhead"])
     para_score = []
     for para in text["article"]:
-        para_score.append(analyze_sentiment_(para))
+        para_score.append(analyze_sentiment(para))
         pbar.update(1)
     scores.append(0.2 * title_score + 0.1 * subhead_score + 0.7 * sum(para_score)/len(para_score))
 print("Program Execution Time: "+str(time.time()-start)+" seconds. "+str(count)+" paragraphs has been analyzed")
